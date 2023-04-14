2118e948bee1c9788a1957140373c4c4f141a4eb
Pc World <samaralashi7@gmail.com>
2023-04-15 02:28:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mobilecloudapplication/AvailableTopics.java
@@ -0,0 +1,13 @@
+package com.example.mobilecloudapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvailableTopics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_topics);
+    }
+}
